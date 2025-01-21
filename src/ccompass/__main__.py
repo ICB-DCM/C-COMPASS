9969449e6d6eb569bc0a0f04b3a36335263aff71
Daniel Weindl <dweindl@users.noreply.github.com>
2025-01-21 17:29:16 +0100

    Add CLI: --version (#119)
    * Add CLI: --version
    
    Add basic command line interface for help and version info.
    
    * Test with pyinstaller bundles

--- src/ccompass/__main__.py
+++ src/ccompass/__main__.py
@@ -1,13 +1,32 @@
 """Package entry-point."""
 
-import FreeSimpleGUI as sg
+import argparse
 
-from .CCMPS import MainController
-from .core import SessionModel
+from . import app_name
 
 
 def main():
     """The entry point for the C-COMPASS application."""
+    from importlib.metadata import version
+
+    parser = argparse.ArgumentParser(description="C-COMPASS application")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"{app_name} {version('ccompass')}",
+    )
+    parser.parse_args()
+
+    launch_gui()
+
+
+def launch_gui():
+    """Launch the C-COMPASS GUI."""
+    import FreeSimpleGUI as sg
+
+    from .CCMPS import MainController
+    from .core import SessionModel
+
     sg.theme("Dark Blue 3")
 
     model = SessionModel()
