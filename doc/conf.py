3724159424c623382e1096ec54a804e8e4d14e4e
Daniel Weindl <daniel.weindl@uni-bonn.de>
2025-02-27 10:56:26 +0100

    doc: fix version number; add sphinx-hoverxref

--- doc/conf.py
+++ doc/conf.py
@@ -3,22 +3,43 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import warnings
+
+import setuptools_scm
+
+# for hoverxref<=1.5.0
+warnings.filterwarnings(
+    "ignore",
+    category=DeprecationWarning,
+    message="The '_Opt' object tuple interface is deprecated, use attribute access instead for 'default', 'rebuild', and 'valid_types'",
+)
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "C-COMPASS"
 copyright = "2024, Daniel Haas"
 author = "Daniel Haas"
-release = "1.0.0"
+version = release = setuptools_scm.get_version(root="..", relative_to=__file__)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["hoverxref.extension"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# hoverxref configuration
+hoverxref_auto_ref = True
+hoverxref_roles = [
+    "term",
+]
+hoverxref_role_types = {
+    "hoverxref": "tooltip",
+    "ref": "tooltip",
+    "term": "tooltip",
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
