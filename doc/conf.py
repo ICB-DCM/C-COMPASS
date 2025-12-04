32fa2baae0b0093ee22a4fa2e73fc999398501b9
Daniel Weindl <dweindl@users.noreply.github.com>
2025-12-04 14:57:39 +0100

    doc: Remove obsolete sphinx-hoverxref (#201)

--- doc/conf.py
+++ doc/conf.py
@@ -3,16 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import warnings
 
 import setuptools_scm
-
-# for hoverxref<=1.5.0
-warnings.filterwarnings(
-    "ignore",
-    category=DeprecationWarning,
-    message="The '_Opt' object tuple interface is deprecated, use attribute access instead for 'default', 'rebuild', and 'valid_types'",
-)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -25,21 +17,10 @@ version = release = setuptools_scm.get_version(root="..", relative_to=__file__)
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["hoverxref.extension"]
+extensions = []
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# hoverxref configuration
-hoverxref_auto_ref = True
-hoverxref_roles = [
-    "term",
-]
-hoverxref_role_types = {
-    "hoverxref": "tooltip",
-    "ref": "tooltip",
-    "term": "tooltip",
-}
 
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-the-linkcheck-builder
 linkcheck_anchors_ignore_for_url = [
